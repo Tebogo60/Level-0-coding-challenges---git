f22fe1503280c69b42f96369e551a00e7062fdb8
Tebogo60 <91907261+Tebogo60@users.noreply.github.com>
2021-10-21 15:20:06 +0200

    Task 0.8 Complete

--- TASK08.js
+++ TASK08.js
@@ -1,27 +1,13 @@
 function numToHourMin(num) {
+    hour = Math.floor(num/60);
+    minutes = num%60;
 
-    if (num <= 59) {
-        console.log(num + " minutes");
-    }
-    else if (num == 60) {
-        console.log(1 + " hour");
-    }
-    else if (num > 60 && num < 119) {
-        return (1 + " hour and " + " minutes");
-    }
-    else if (num == 120) {
-        console.log(2 + " hours");
-    }
-    else if (num > 180 && num < 239) {
-        console.log(3 + " hours and " + " minutes");
-    }
-    else if (num == 240) {
-        console.log(4 + " Hours");
-    }
-    else if (num > 240 && num < 399) {
-        console.log(4 + " hours and " + " minutes")
+    if (h == 1) {
+        return (hour + " Hour " + minutes + " Minutes ");
+    } else {
+        return (hour + " Hours " + minutes + " Minutes ");
     }
 
 }
 
-numToHourMin(246);
+console.log(numToHourMin(145));
