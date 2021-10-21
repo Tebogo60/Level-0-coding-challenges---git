426b3c37d7cb65bfb709f9e393a761c32e26f5e2
Tebogo60 <91907261+Tebogo60@users.noreply.github.com>
2021-10-21 14:45:25 +0200

    Task 0.7 Complete

---
+++ TASK07.js
@@ -0,0 +1,13 @@
+function fahrenheitToCelsius(celsius) {
+    var fahrenheit = (celsius * 1.8) + 32;
+    console.log(fahrenheit);
+}
+
+fahrenheitToCelsius(25);
+
+function celsiusToFahrenheit(fahrenheit) {
+    var celsius = (fahrenheit - 32) * 5 / 9
+        console.log(celsius);
+}
+
+celsiusToFahrenheit(80);
