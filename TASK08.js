638c52adb3e2f60ff9f85411c348da34b04e959f
Tebogo60 <91907261+Tebogo60@users.noreply.github.com>
2021-10-21 14:51:45 +0200

    TASK08.js something wrong

---
+++ TASK08.js
@@ -0,0 +1,19 @@
+function numToHourMin(num) {
+
+    if (num <= 59) {
+        console.log(num + " minutes");
+    }
+    else if (num == 60) {
+        console.log(1 + " hour");
+    }
+    else if (num > 60 && num < 119) {
+        return (1 + " hour and " + " minutes");
+    }
+    else if (num == 120) {
+        console.log(2 + " hours");
+    }
+    else if (num > 180 && num < 239) {
+        console.log(3 + " hours" + " minutes");
+    }
+
+}
