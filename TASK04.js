8b84c72b3899d735e8612aeb4b5a06536d3cba4a
Tebogo60 <91907261+Tebogo60@users.noreply.github.com>
2021-10-21 14:39:06 +0200

    TASK 04 Complete

---
+++ TASK04.js
@@ -0,0 +1,15 @@
+function evenOrOdd(number) {
+
+    if(number == 3) {
+        console.log(number + " Odd!");
+    }
+    else if(number == 4) {
+        console.log(number + " even!");
+    }
+    else {
+        console.log(number + "(Number must be 3 or 4)")
+    }
+
+}
+
+evenOrOdd(2);
