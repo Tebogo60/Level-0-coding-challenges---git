b81268a58989bb986dba5d982a4fe20f58d35d83
Tebogo60 <91907261+Tebogo60@users.noreply.github.com>
2021-10-27 03:58:32 +0200

    I was able to fix the error and now the code can handle singulars and plurals for both hours and minutes.

--- TASK08.js
+++ TASK08.js
@@ -2,12 +2,29 @@ function numToHourMin(num) {
     hour = Math.floor(num/60);
     minutes = num%60;
 
-    if (h == 1) {
-        return (hour + " Hour " + minutes + " Minutes ");
-    } else {
-        return (hour + " Hours " + minutes + " Minutes ");
+    if (hour == 1 && minutes == 1) {
+        return (hour + " Hour, " + minutes + " Minute");
+    }
+    else if (hour > 1 && minutes == 1) {
+        return (hour + " Hours, " + minutes + " Minute");
+    }
+    else if (hour == 1 && minutes >= 2) {
+        return (hour + " Hour, " + minutes + " Minutes");
     }
 
+    else if (hour < 1 && minutes == 0) {
+        return (minutes + " Minute");
+    }
+    else if (hour < 1 && minutes == 1) {
+        return (minutes + " Minute");
+    }
+    else if (hour < 1 && minutes >= 2) {
+        return (minutes + " Minutes");
+    }
+
+    else {
+        return (hour + " Hours, " + minutes + " Minutes ");
+    }
 }
 
-console.log(numToHourMin(145));
+console.log(numToHourMin(133));
