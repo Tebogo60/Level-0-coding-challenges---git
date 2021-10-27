7d561e912d3b29c1bda1f633246e1f7ac1e8de7b
Tebogo60 <91907261+Tebogo60@users.noreply.github.com>
2021-10-27 14:18:09 +0200

    Task0.6 Complete

--- TASK06.js
+++ TASK06.js
@@ -1,13 +1,14 @@
 function maximum(a, b, c) {
+
     if (a >= b && a >= c) {
-        return console.log(a);
+        return a;
     }
     else if (b >= a && b >= c) {
-        return console.log(b);
+        return b;
     }
     else if (c >= a && c >= b) {
-        return console.log(c);
+        return c;
     }
 }
 
-maximum(2, 3, 4);
+console.log(maximum(3, 4, 5));
