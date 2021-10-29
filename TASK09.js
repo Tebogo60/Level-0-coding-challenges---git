1a0d11f86b390213d4e2bcb15e1e6088b38840c2
Tebogo60 <91907261+Tebogo60@users.noreply.github.com>
2021-10-29 02:58:36 +0200

    Complete!

--- TASK09.js
+++ TASK09.js
@@ -1,11 +1,15 @@
 function printVowels(string) {
-    let vows = ("aeiouAEIOU");
+
+    let vows = "aeiou";
     let stringVowels = "";
 
     for(let i = 0; i <= string.length; i++) {
-        if (vows.includes(string[i])) {
-            stringVowels = stringVowels + string[i] + ", ";
+        if (vows.includes(string.toLowerCase()[i])) {
+            stringVowels = stringVowels + string[i] + ",";
         }
     }
-    return (stringVowels);
-}  console.log(printVowels("Umuzi Academy"));
+    console.log(...new Set(stringVowels.toLowerCase()));
+}  
+
+printVowels("Umuzi");
+
