e73ff0c47116788b94ddd6bca3b63c928730918f
Tebogo60 <91907261+Tebogo60@users.noreply.github.com>
2021-10-29 03:00:36 +0200

    Complete!

--- TASK10.js
+++ TASK10.js
@@ -1,12 +1,15 @@
 function commonLetters() {
-    let input1 = ("umuzi");
-    let input2 = "Umuzi Academy";
+    let input1 = "free";
+    let input2 = "three";
     let commonLetters = "";
 
     for(let i = 0; i <= input2.length; i++) {
         if (input1.includes(input2[i])) {
-            commonLetters = commonLetters + input2[i] + ", ";
+            commonLetters = commonLetters + input2[i] + ",";
         }
     }
-    return (commonLetters);
-}  console.log(commonLetters());
+
+    console.log(...new Set(commonLetters));
+} 
+
+commonLetters();
