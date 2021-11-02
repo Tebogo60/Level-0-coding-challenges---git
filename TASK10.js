eaec95249a956d32623f5c26faf838dc68bb7798
Tebogo60 <91907261+Tebogo60@users.noreply.github.com>
2021-11-02 15:39:49 +0000

    Update TASK10.js

--- TASK10.js
+++ TASK10.js
@@ -1,15 +1,16 @@
 function commonLetters() {
     let input1 = "free";
     let input2 = "three";
-    let commonLetters = "";
+    let results = "";
 
     for(let i = 0; i <= input2.length; i++) {
         if (input1.includes(input2.toLowerCase()[i])) {
-            commonLetters = commonLetters + input2[i] + ",";
+            results = results + input2[i];
+            results = [...new Set(results)];
         }
     }
-
-    console.log(...new Set(commonLetters));
+    console.log("Common Letters: " + results.join(''));
+    
 } 
 
 commonLetters();
