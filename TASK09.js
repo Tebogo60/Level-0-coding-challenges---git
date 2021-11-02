e9250154cbc516ccbfc27dd7580889e1738ccf43
Tebogo60 <91907261+Tebogo60@users.noreply.github.com>
2021-11-02 15:40:34 +0000

    Update TASK09.js

--- TASK09.js
+++ TASK09.js
@@ -1,14 +1,14 @@
 function printVowels(string) {
-    let vows = "aeiou";
-    let stringVowels = "";
+    let vows = "aeiou".split([""]);
+    let allVowelsInString = "";
 
-    for(let i = 0; i <= string.length; i++) {
-        if (vows.includes(string.toLowerCase()[i])) {
-            stringVowels = stringVowels + string[i] + ",";
+    for(let i = 0; i < string.length; i++) {
+        if(vows.includes(string.toLowerCase()[i])) {
+            allVowelsInString += string.toLowerCase()[i];
+            allVowelsInString = [...new Set(allVowelsInString)];
         }
     }
-    console.log(...new Set(stringVowels.toLowerCase()));
-}  
+    console.log("Vowels: " + allVowelsInString);
+}
 
 printVowels("Umuzi");
-
